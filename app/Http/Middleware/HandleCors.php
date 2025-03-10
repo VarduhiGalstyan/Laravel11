17dccd30ca38e2057145de8247135ce1303e313c
Varduhi <varduhigalstyann71@gmail.com>
2025-03-10 16:05:50 +0400

    Api CORS+

---
+++ app/Http/Middleware/HandleCors.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HandleCors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        return [
+    'paths' => ['api/*'], 
+    'allowed_methods' => ['*'], 
+    'allowed_origins' => ['*'], 
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
+];
+
+    }
+}
